e43badf544c3c24ad8d4f8efa75f98626b63fbdc
Mythily S <mythily.s@maersk.com>
2021-08-08 21:46:36 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http:HttpClient) {}
   title = 'postmethod';
+  onSubmit(data:any){
+     
+     this.http.post('http://localhost:3000/postserver',data)
+     .subscribe((result)=>{
+       console.log("result",result)
+   })
+   console.log(data);
 }
+}
+
